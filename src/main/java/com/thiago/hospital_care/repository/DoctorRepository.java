dd367b5988132949dc40036e28fb4ed92ad593d4
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-02-26 17:47:02 -0300

    Criar repositories

---
+++ src/main/java/com/thiago/hospital_care/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.thiago.hospital_care.repository;
+
+import com.thiago.hospital_care.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
