45c8f72891b56ffdb6c3a249762b5886d5983c57
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-03-06 20:07:56 -0300

    Refatorar DTOs de update

--- src/main/java/com/thiago/hospital_care/dto/AppointmentUpdateDTO.java
+++ src/main/java/com/thiago/hospital_care/dto/AppointmentUpdateDTO.java
@@ -1,6 +1,6 @@
 package com.thiago.hospital_care.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,12 +9,14 @@ public class AppointmentUpdateDTO {
 
     private Long id;
 
-    @NotBlank(message = "Data e hora não podem ser nulos/vazios.")
+    @Pattern(
+            regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])T([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)$",
+            message = "Data e hora inválidas. Use o formato yyyy-MM-dd'T'HH:mm:ss"
+    )
     private String dateTime;
 
     @Size(max = 300, message = "São permitidos no máximo 300 caracteres.")
     private String description;
 
-    @NotBlank(message = "Status não pode ser nulo/vazio.")
     private String status;
 }
--- src/main/java/com/thiago/hospital_care/dto/DoctorUpdateDTO.java
+++ src/main/java/com/thiago/hospital_care/dto/DoctorUpdateDTO.java
@@ -1,6 +1,5 @@
 package com.thiago.hospital_care.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,10 +10,8 @@ public class DoctorUpdateDTO extends UserUpdateDTO{
 
     private Long id;
 
-    @NotBlank(message = "CRM não pode ser nulo/vazio.")
     private String crm;
 
-    @NotBlank(message = "Especialidade não pode estar vazia.")
     @Pattern(
             regexp = "^(Cardiologia|Ortopedia|Pediatria|Dermatologia|Ginecologia|Urologia|Clínico geral)$",
             message = "Especialidade inválida."
